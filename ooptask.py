4d8067be6a1ececab408943c945d1a8b205aed4f
Abimbola <jasperbimbo@gmail.com>
2022-10-16 23:16:41 +0100

    Initial commit

---
+++ ooptask.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    length = 2
+    breadth =2
+
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+    print("perimeter(length=",self.length,", breadth=", self.breadth)
+
+
+    perimeter = Rectangle("2l"+"2b")
+
+
